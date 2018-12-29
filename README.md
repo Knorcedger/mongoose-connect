5a72f90d55ae2df4010870b27819ace5e574dfdc
Achilleas Tsoumitas <knorcedger@gmail.com>
2018-12-29 19:32:00 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# mongoose-connect
+
+A module to avoid mongoose connect boilerplate. Helps to easily connect to a mongoose database and handle error, reconnections etc.
+
+### Installation
+```
+npm install @knorcedger/mongoose-connect
+```
+
+### Usage
+```javascript
+const mongoose = require('mongoose');
+const mongooseConnect = require('@knorcedger/mongoose-connect');
+
+const dbUrl = 'mongodb://user:password@host:port/database';
+
+mongooseConnect(mongoose, dbUrl);
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+const signale = require('signale');
+
+module.exports = (mongoose, url) => new Promise((resolve, reject) => {
+  mongoose.connection.on('connected', () => {
+    signale.info('Connection Established');
+    resolve();
+  });
+
+  mongoose.connection.on('reconnected', () => {
+    signale.info('Connection Reestablished');
+  });
+
+  mongoose.connection.on('disconnected', () => {
+    signale.info('Connection Disconnected');
+  });
+
+  mongoose.connection.on('close', () => {
+    signale.info('Connection Closed');
+  });
+
+  mongoose.connection.on('error', error => {
+    signale.info(`ERROR: ${error}`);
+    reject();
+  });
+
+  const dbName = url.substr(url.lastIndexOf('/') + 1, url.length);
+
+  signale.info(`Connecting to database: ${dbName}`);
+  mongoose.set('useCreateIndex', true);
+  mongoose.set('useNewUrlParser', true);
+  mongoose.connect(url, {
+    autoReconnect: true,
+    reconnectTries: 1000000,
+    reconnectInterval: 3000
+  });
+});
