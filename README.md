8b352316f20e2837363ee50b6a82247ba9a05e57
Achilleas Tsoumitas <knorcedger@gmail.com>
2021-07-01 11:42:27 +0300

    Add useNewUrlParser

--- README.md
+++ README.md
@@ -3,16 +3,18 @@
 A module to avoid mongoose connect boilerplate. Helps to easily connect to a mongoose database and handle error, reconnections etc.
 
 ### Installation
+
 ```
 npm install @knorcedger/mongoose-connect
 ```
 
 ### Usage
-```javascript
-const mongoose = require('mongoose');
-const mongooseConnect = require('@knorcedger/mongoose-connect');
 
-const dbUrl = 'mongodb://user:password@host:port/database';
+```javascript
+const mongoose = require("mongoose");
+const mongooseConnect = require("@knorcedger/mongoose-connect");
+
+const dbUrl = "mongodb://user:password@host:port/database";
 
 mongooseConnect(mongoose, dbUrl);
 ```
--- index.js
+++ index.js
@@ -1,46 +1,51 @@
-const signale = require('signale');
+const signale = require("signale");
 
-module.exports = (mongoose, url) => new Promise((resolve, reject) => {
-  mongoose.connection.on('connected', () => {
-    signale.info('Connection Established');
-    resolve('Connected');
+module.exports = (mongoose, url) =>
+  new Promise((resolve, reject) => {
+    mongoose.connection.on("connected", () => {
+      signale.info("Connection Established");
+      resolve("Connected");
+    });
+
+    mongoose.connection.on("reconnected", () => {
+      signale.info("Connection Reestablished");
+    });
+
+    mongoose.connection.on("disconnected", () => {
+      signale.info("Connection Disconnected");
+    });
+
+    mongoose.connection.on("close", () => {
+      signale.info("Connection Closed");
+    });
+
+    mongoose.connection.on("error", (error) => {
+      signale.info(`ERROR: ${error}`);
+      reject(error);
+    });
+
+    if (!mongoose) {
+      throw new Error("Please provide a valid mongoose instance");
+    }
+
+    if (!url) {
+      throw new Error("Please provide a valid database url");
+    }
+
+    const dbName = url.substring(
+      url.lastIndexOf("/") + 1,
+      url.indexOf("?") !== -1 ? url.indexOf("?") : url.length
+    );
+
+    signale.info(`Connecting to database: ${dbName}`);
+    mongoose.set("useCreateIndex", true);
+    mongoose.set("useNewUrlParser", true);
+    mongoose.set("useFindAndModify", false);
+
+    return mongoose.connect(url, {
+      autoReconnect: true,
+      reconnectTries: 1000000,
+      reconnectInterval: 3000,
+      useNewUrlParser: true,
+    });
   });
-
-  mongoose.connection.on('reconnected', () => {
-    signale.info('Connection Reestablished');
-  });
-
-  mongoose.connection.on('disconnected', () => {
-    signale.info('Connection Disconnected');
-  });
-
-  mongoose.connection.on('close', () => {
-    signale.info('Connection Closed');
-  });
-
-  mongoose.connection.on('error', error => {
-    signale.info(`ERROR: ${error}`);
-    reject(error);
-  });
-
-  if (!mongoose) {
-    throw new Error('Please provide a valid mongoose instance');
-  }
-
-  if (!url) {
-    throw new Error('Please provide a valid database url');
-  }
-
-  const dbName = url.substring(url.lastIndexOf('/') + 1, url.indexOf('?') !== -1 ? url.indexOf('?') : url.length);
-
-  signale.info(`Connecting to database: ${dbName}`);
-  mongoose.set('useCreateIndex', true);
-  mongoose.set('useNewUrlParser', true);
-  mongoose.set('useFindAndModify', false);
-
-  return mongoose.connect(url, {
-    autoReconnect: true,
-    reconnectTries: 1000000,
-    reconnectInterval: 3000
-  });
-});
